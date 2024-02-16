ecc82f7d08961d1aaee02fdc462e08cef427f414
anasmustafa69 <160245169+anasmustafa69@users.noreply.github.com>
2024-02-17 01:56:39 +0500

    Add files via upload

---
+++ main17.ts
@@ -0,0 +1,18 @@
+let places= ["lahore","japan", "saudia", "kashmir","bangladesh"];
+console.log("original order",places);
+console.log("alphabetical order",places.sort());
+
+console.log("original order",places);
+console.log("reversing the order", places.reverse());
+
+console.log("original order",places);
+
+places.reverse();
+console.log("reverse order",places);
+
+places.reverse();
+console.log("original order",places);
+
+console.log("sorting alphabetically",places.sort());
+
+console.log("sorting it by reverse order",places.sort().reverse());
