bfbc5436eead52f18394d009b6ef5a4d8032f5f1
anasmustafa69 <160245169+anasmustafa69@users.noreply.github.com>
2024-02-17 04:52:13 +0500

    Add files via upload

---
+++ main36.ts
@@ -0,0 +1,6 @@
+function makeShirt(size = "large", message = "I love TypeScript") {
+    console.log(`The shirt size is ${size} and the message printed on it is: "${message}".`);
+}
+makeShirt();
+makeShirt("medium");
+makeShirt("small", "JavaScript is fun!");
