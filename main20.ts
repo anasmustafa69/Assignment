a18b3e670ee592f80f27539d844559b05c6a63ff
anasmustafa69 <160245169+anasmustafa69@users.noreply.github.com>
2024-02-17 02:26:26 +0500

    Add files via upload

---
+++ main20.ts
@@ -0,0 +1,12 @@
+let places  = ["china", "turkey", "italy", "india", "pakistan"];
+console.log(places);
+let pakistan = {
+    capital: "islamabad",
+    foundationYear: 1947
+};
+
+let india = {
+    capital: "dehli",
+    foundationYear: 1947
+};
+console.log(india);
