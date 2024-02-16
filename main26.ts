f08baadc3b0720ee9c57b613963a519a335dc745
anasmustafa69 <160245169+anasmustafa69@users.noreply.github.com>
2024-02-17 04:00:48 +0500

    Add files via upload

---
+++ main26.ts
@@ -0,0 +1,8 @@
+let alien = 'red';
+if (alien == 'green' ){
+    console.log("congratulation, youn won 5 points");
+}else if(alien=='yellow'){
+    console.log("congratulation, youn won 10 points");
+}else if(alien=='red'){
+    console.log("congratulation, youn won 10 points");
+}
