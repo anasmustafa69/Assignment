adea3ffd550c33c847eb4597f874ef86b3b55299
anasmustafa69 <160245169+anasmustafa69@users.noreply.github.com>
2024-02-17 04:46:17 +0500

    Add files via upload

---
+++ main35.ts
@@ -0,0 +1,3 @@
+function makeShirt(size, message) {
+    console.log("The shirt size is " + size + " and the message printed on it is: \"" + message + "\".");
+}makeShirt("L", "eat, code and sleep");
