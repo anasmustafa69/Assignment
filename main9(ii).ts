50e21db49d13536d1e46c611df5c457bf20be6a2
anasmustafa69 <160245169+anasmustafa69@users.noreply.github.com>
2024-02-16 22:15:40 +0500

    Add files via upload

---
+++ main9(ii).ts
@@ -0,0 +1,5 @@
+
+let famous_person= "Albert Einstein";
+let message= `${famous_person} once said, "A person who never made a mistake never tried anything new."`;
+console.log(message);
+//in this program we are writing a famour quote of Albert Einstein
