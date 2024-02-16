132e5fe2ce40608e4f37ca9d9654a63c3e057f54
anasmustafa69 <160245169+anasmustafa69@users.noreply.github.com>
2024-02-17 03:12:24 +0500

    Add files via upload

---
+++ main22.ts
@@ -0,0 +1,29 @@
+let x = 5;
+let y = 10;
+
+console.log("Is x equal to 5? I predict True.");
+console.log(x == 5);
+console.log("Is y equal to 10? I predict True.");
+console.log(y == 10);
+
+console.log("Is x not equal to y? I predict True.");
+console.log(x != y);
+
+console.log("Is x less than y? I predict True.");
+console.log(x < y);
+
+console.log("Is y greater than x? I predict True.");
+console.log(y > x);
+console.log("Is x equal to y? I predict False.");
+console.log(x == y);
+
+ console.log("Is y less than x? I predict False.");
+console.log(y < x);
+
+console.log("Is x greater than y? I predict False.");
+console.log(x > y);
+  
+console.log("Is x not equal to 5? I predict False.");
+console.log(x != 5);
+console.log("Is y equal to 100? I predict False.");
+console.log(y == 100);
