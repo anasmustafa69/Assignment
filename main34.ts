ccec252497f019736b50f716c6ee3931015e64a6
anasmustafa69 <160245169+anasmustafa69@users.noreply.github.com>
2024-02-17 04:36:35 +0500

    Add files via upload

---
+++ main34.ts
@@ -0,0 +1,8 @@
+let animals  = ["dog", "cat", "rabbit"];
+console.log("Printing animal names:");
+for (let animal of animals) {
+    console.log(animal);
+}console.log("\nPrinting statements about each animal:");
+for (let animal of animals) {
+    console.log(`A ${animal} would make a great pet.`);
+}console.log("\nAny of these animals would make a great pet!");
