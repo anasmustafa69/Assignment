18012815d33e90c6e14a1a6f53d87a53dedd53e4
anasmustafa69 <160245169+anasmustafa69@users.noreply.github.com>
2024-02-17 04:30:34 +0500

    Add files via upload

---
+++ main33.ts
@@ -0,0 +1,9 @@
+let favorite_pizzas= ["Pepperoni", "Margherita", "BBQ Chicken"];
+
+console.log("Printing pizza names:");
+for (let pizza of favorite_pizzas) {
+    console.log(pizza);
+}console.log("\nPrinting pizza statements:");
+for (let pizza of favorite_pizzas) {
+    console.log(`I like ${pizza} pizza.`);
+}console.log("\nI really love pizza!");
