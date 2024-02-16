e545e0d0da4f48e64efa0603139daee33003eafa
anasmustafa69 <160245169+anasmustafa69@users.noreply.github.com>
2024-02-16 21:45:39 +0500

    Add files via upload

---
+++ main9.ts
@@ -0,0 +1,4 @@
+console.log("addition", 5+3 );//Anas
+console.log("subtract", 11-3);//2/16/2024
+console.log("multiply", 4*2);//In this program we are performing some mathametical operations.
+console.log("division", 16/2);
