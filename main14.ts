6739da92957a7ff15341f12dd47c39736bc1d08a
anasmustafa69 <160245169+anasmustafa69@users.noreply.github.com>
2024-02-17 00:06:11 +0500

    Add files via upload

---
+++ main14.ts
@@ -0,0 +1,8 @@
+let guestlist =["Wajid", "Sohail", "Rehan", "Bilal"];
+for (let i=0; i<=3; i++)
+console.log(guestlist[i]," You're invited for dinner at my home ");
+console.log("bilal cant make to come at the dinner");
+guestlist[3]= "Anas" ;
+let guestlist2 =["Wajid", "Sohail", "Rehan", "Anas"];
+for (let i=0; i<=3; i++)
+console.log(guestlist2[i]," You're invited for dinner at my home ");
