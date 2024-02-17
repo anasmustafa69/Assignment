9cc83b6471d07fe5741bd85054511328896f893c
anasmustafa69 <160245169+anasmustafa69@users.noreply.github.com>
2024-02-17 05:18:37 +0500

    Add files via upload

---
+++ main40.ts
@@ -0,0 +1,9 @@
+function show_magicians(magicians )  {
+    for (let magician of magicians) {
+        console.log(magician);
+    }
+}
+
+ 
+let magicians  = ["Merlin", "David Copperfield", "Houdini", "Penn & Teller"];
+show_magicians(magicians);
