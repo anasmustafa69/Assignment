cd694186226804d0a453bb74ef2ee95d87cc73ed
anasmustafa69 <160245169+anasmustafa69@users.noreply.github.com>
2024-02-22 12:45:37 +0500

    Update main3.ts

--- main3.ts
+++ main3.ts
@@ -1,2 +1,2 @@
-let famous_person = William shakespear;
-console.log('William shakespear once said,"Be not afraid of greatness. Some are born great, some achieve greatness, and others have greatness thrust upon them."');
+let famous_person = "William_shakespear";
+console.log(famous_person,' once said,"Be not afraid of greatness. Some are born great, some achieve greatness, and others have greatness thrust upon them."');
