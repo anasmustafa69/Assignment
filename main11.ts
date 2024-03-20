38ed926f6187b1539f370f23a83389f106044e89
anasmustafa69 <160245169+anasmustafa69@users.noreply.github.com>
2024-03-21 02:42:33 +0500

    Update main11.ts

--- main11.ts
+++ main11.ts
@@ -1,3 +1,3 @@
 let names=["Anas, wakeup early in the morning.", "qasim, eat well.", "jamal, enjoy your life.", "sohail, study properly."];
 for (let i=0; i<=3; i++)
-console.log(names[i]);
+console.log("hi",names[i]);
