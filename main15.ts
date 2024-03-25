fdf9b47c4a7045f7f9ed1d303146cca6def0d437
anasmustafa69 <160245169+anasmustafa69@users.noreply.github.com>
2024-03-25 20:43:07 +0500

    Update main15.ts

--- main15.ts
+++ main15.ts
@@ -4,15 +4,16 @@ console.log(guestlist[i]," You're invited for dinner at my home ");
 console.log("bilal cant make to come at the dinner");
 guestlist[3]= "Anas" ;
 let guestlist2 =["Wajid", "Sohail", "Rehan", "Anas"];
-for (let i=0; i<=3; i++)
+for (let i=0; i<=7; i++)
 console.log(guestlist2[i]," You're invited for dinner at my home ");
 console.log("Great news! I found a bigger dinner table.");
-guestlist = ["Sir Ishaq", ...guestlist];
-console.log(guestlist);
-guestlist.splice("usman");
+guestlist2 = ["Sir Ishaq", ...guestlist2];
 
-guestlist.push("ismail");
+guestlist2.push("usman");
+guestlist2.push("ismail");
 
-guestlist.forEach(guestlist => {
-    console.log(`Dear ${guestlist}, you are cordially invited to dinner`);
+console.log(guestlist2);
+
+guestlist2.forEach(guestlist2 => {
+    console.log(`Dear ${guestlist2}, you are cordially invited to dinner.`);
 });
