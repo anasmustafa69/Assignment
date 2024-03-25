0064017b1224240f225f50564417870d69d28665
anasmustafa69 <160245169+anasmustafa69@users.noreply.github.com>
2024-03-26 03:25:53 +0500

    Update main44.ts

--- main44.ts
+++ main44.ts
@@ -3,7 +3,6 @@ function storeCarInfo(manufacturer, modelName, ...options) {
         manufacturer: manufacturer,
         modelName: modelName
     };
-    // Assign additional options to the carInfo Object
     for (let option of options) {
         for (let key in option) {
             carInfo[key] = option[key];
@@ -12,8 +11,5 @@ function storeCarInfo(manufacturer, modelName, ...options) {
     return carInfo;
 }
 
-// Call the function with required information and two other name-value pairs
 let car = storeCarInfo("Toyota", "Camry", { color: "red" }, { year: 2022 });
-
-// Print the Object returned by the function
 console.log(car);
