0ffdf6ff7afcf6f1637c7a90b7244caa0fe22524
anasmustafa69 <160245169+anasmustafa69@users.noreply.github.com>
2024-03-25 22:25:40 +0500

    Update main16.ts

--- main16.ts
+++ main16.ts
@@ -7,23 +7,25 @@ let guestlist2 =["Wajid", "Sohail", "Rehan", "Anas"];
 for (let i=0; i<=3; i++)
 console.log(guestlist2[i]," You're invited for dinner at my home ");
 console.log("Great news! I found a bigger dinner table.");
-guestlist = ["Sir Ishaq", ...guestlist];
-console.log(guestlist);
-guestlist.splice("usman");
+guestlist2 = ["Sir Ishaq", ...guestlist2];
+guestlist2.push("usman");
+guestlist2.push("ismail");
+console.log(guestlist2);
 
-guestlist.push("ismail");
-
-guestlist.forEach(guestlist => {
-    console.log(`Dear ${guestlist}, you are cordially invited to dinner`);
+guestlist2.forEach(guestlist2 => {
+    console.log(`Dear ${guestlist2}, you are cordially invited to dinner.`);
 });
+
 console.log("Bad news! The new dinner table won't arrive in time, so I can only invite two people for dinner.");
-while (guestlist.length > 2) {
-    let removedGuest = guestList.pop();
-    console.log(`Sorry , I can't invite you to dinner.`);
+while (guestlist2.length > 2) {
+    let removedGuest = guestlist2.pop();
+     
+    console.log(removedGuest,`Sorry , I can't invite you to dinner.`);
 }
 console.log(`Dear ${guestlist[0]}, you are still invited to dinner.`);
 console.log(`Dear ${guestlist[1]}, you are still invited to dinner.`);
 guestlist.pop();
 guestlist.pop();
 
-console.log("Remaining guests:", guestlist);
+console.log("Remaining guests:", guestlist2);
+console.log(`You are inviting ${guestlist2.length} people to dinner.`);
